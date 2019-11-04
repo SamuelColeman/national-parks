6ed0463341cf3e6efb8ecd5fffa3eaa2cbedd98a
Samuel Coleman <smcoleman19@gmail.com>
2019-11-03 22:20:49 -0700

    Add full testing coverage for info container and partial for park card

--- src/Containers/InfoContainer/InfoContainer.test.js
+++ src/Containers/InfoContainer/InfoContainer.test.js
@@ -28,4 +28,45 @@ describe('InfoContainer', () => {
   it('should match snapshot with correct data passing through', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should match snapshot with correct data passing through', () => {
+  	mockParkInfo = [];
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('map state to props gives the park info in state', () => {
+		const mockState = { parkInfo: mockParkInfo};
+
+		const expected = {
+			parkInfo: mockState.parkInfo
+		};
+
+		const mappedState = mapStateToProps(mockState);
+
+		expect(mappedState).toEqual(expected);
+	});
+
+	it('map state to props gives the info name in state', () => {
+		const mockState = { infoName: mockInfoName};
+
+		const expected = {
+			infoName: mockState.infoName
+		};
+
+		const mappedState = mapStateToProps(mockState);
+
+		expect(mappedState).toEqual(expected);
+	});
+
+	it('map state to props gives the park id in state', () => {
+		const mockState = { parkId: mockParkId};
+
+		const expected = {
+			parkId: mockState.parkId
+		};
+
+		const mappedState = mapStateToProps(mockState);
+
+		expect(mappedState).toEqual(expected);
+	});
 });
