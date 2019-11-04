d4d64ae873c0d350937f8f950bd01083aee81c2c
Samuel Coleman <smcoleman19@gmail.com>
2019-11-04 15:47:38 -0700

    Add basic styling and coloring for specific park page

--- src/Containers/ParkCard/ParkCard.js
+++ src/Containers/ParkCard/ParkCard.js
@@ -17,29 +17,35 @@ export const ParkCard = ({ name, designation, id, parks, fullName, description, 
 		)
 	} else {
 		return (
-			<section>
-				<h1 className='park_card_title'>{fullName}</h1>
-				<h1 className='park_card_p'>Overview:</h1>
-				<h2 className='park_card_text'>{description}</h2>
-				<h1 className='park_card_p'>Weather:</h1>
-				<h2 className='park_card_text'>{weatherInfo}</h2>
-				<section onClick={(e) => displayParkInfo(parkCode, e)}>
-					<Link to={`/parks/${id}/VisitorCenters`}>
-						<button name='VisitorCenters'>Visitor Centers</button>
-					</Link>
-					<Link to={`/parks/${id}/Events`}>
-						<button name='Events'>Events</button>
-					</Link>
-					<Link to={`/parks/${id}/Alerts`}>
-						<button name='Alerts'>Alerts</button>
-					</Link>
-					<Link to={`/parks/${id}/Campgrounds`}>
-						<button name='Campgrounds'>Campgrounds</button>
+			<section className='park_section'>
+				<section className='park_card_header'>
+					<h1 className='park_card_title'>{fullName}</h1>
+				</section>
+				<section className='park_card_info'>
+					<h1 className='park_card_p'>Overview:</h1>
+					<h2 className='park_card_text'>{description}</h2>
+					<h1 className='park_card_p'>Weather:</h1>
+					<h2 className='park_card_text'>{weatherInfo}</h2>
+				</section>
+				<section className='park_card_btns'>
+					<section onClick={(e) => displayParkInfo(parkCode, e)}>
+						<Link to={`/parks/${id}/VisitorCenters`}>
+							<button className='info_name' name='VisitorCenters'>Visitor Centers</button>
+						</Link>
+						<Link to={`/parks/${id}/Events`}>
+							<button className='info_name' name='Events'>Events</button>
+						</Link>
+						<Link to={`/parks/${id}/Alerts`}>
+							<button className='info_name' name='Alerts'>Alerts</button>
+						</Link>
+						<Link to={`/parks/${id}/Campgrounds`}>
+							<button className='info_name' name='Campgrounds'>Campgrounds</button>
+						</Link>
+					</section>
+					<Link to='/parks'>
+						<button className='info_name'>Back</button>
 					</Link>
 				</section>
-				<Link to='/parks'>
-					<button>Back</button>
-				</Link>
 			</section>
 		)
 	}
